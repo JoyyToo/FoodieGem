52b50fcd9d9f0e0840592d1768bdfd5cbf7738a6
joyytoo <joytoo97@gmail.com>
2019-03-15 19:16:22 +0300

    Add generator

--- lib/foodie_tool/cli.rb
+++ lib/foodie_tool/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'foodie_tool'
+require 'foodie_tool/generators/recipe'
 
 module FoodieTool
   class CLI < Thor
@@ -12,6 +13,11 @@ module FoodieTool
     method_option :word, aliases: '-w'
     def pluralize
       puts FoodieTool::Food.pluralize(options[:word])
+    end
+
+    desc 'recipe', 'Generates a recipe scaffold'
+    def recipe(group, name)
+      FoodieTool::Generators::Recipe.start([group, name])
     end
   end
 end
